74759d02c4fb9d629173b25eefee85bbc25cb470
Eli Doran <eli@elidoran.com>
2015-12-05 13:28:19 -0500

    add simple example using defaults

---
+++ examples/simple/package.js
@@ -0,0 +1,25 @@
+Package.describe({
+  name: 'simple',
+  version: '1.0.0',
+  summary: 'Sample package using bonce to make its browserified file.',
+  git: '',
+  documentation: 'README.md'
+});
+
+// Note:
+//  there's no Npm.depends() because we don't want Meteor to get the modules
+//  because we don't want them in the published package.
+
+Package.onUse(function(api) {
+  api.versionsFrom('1');
+  api.addFiles([
+    'package.browserified.js'  // this is the file created by bonce
+  ], 'client');
+
+  api.export('upperCase', 'client');
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('simple');
+});
