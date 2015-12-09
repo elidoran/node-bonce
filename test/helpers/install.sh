bf7f43e1c994d84e337af95251799e006a1bbdca
Eli Doran <eli@elidoran.com>
2015-12-09 09:16:21 -0500

    install node modules via a script

---
+++ test/helpers/install.sh
@@ -0,0 +1,11 @@
+cd test/helpers/module-only
+npm install upper-case
+
+cd ../with-input
+npm install upper-case
+
+cd ../with-options
+npm install upper-case
+
+cd ../with-both
+npm install upper-case
